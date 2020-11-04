f5adb7392c6ce068a56f635c5dacf470fb8b1997
alexsandraholanda <alexsandraale8795@gmail.com>
2020-11-04 00:42:14 -0300

    basico rest

--- src/main/java/com/cursoteste/api/MeuResorce.java
+++ src/main/java/com/cursoteste/api/MeuResorce.java
@@ -1,14 +1,35 @@
 package com.cursoteste.api;
 
-import com.sun.javafx.binding.StringFormatter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/clientes")
+
 public class MeuResorce {
 
-    @GetMapping("/api/clientes/{id}")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente Salvar(@RequestBody Cliente cliente){
+        //service.save(cliente);
+        return cliente;
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        //service.buscarPorId(id);
+        //service.delete(cliente);
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        //service.buscarPorId(id);
+        //service.atualizar(cliente);
+    }
+
+    @GetMapping("{id}")
     public Cliente obterDadosCliente( @PathVariable Long id){
         System.out.println(String.format("id recebido via url: %d", id));
         Cliente cliente = new Cliente("teste", "123456");
