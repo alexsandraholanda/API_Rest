dda340e612bad4ca3c811d9ad9d894496ae7c9d1
alexsandraholanda <alexsandraale8795@gmail.com>
2020-11-03 23:51:34 -0300

    Rest parte 1

--- src/main/java/com/cursoteste/api/MeuResorce.java
+++ src/main/java/com/cursoteste/api/MeuResorce.java
@@ -1,9 +1,14 @@
 package com.cursoteste.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MeuResorce {
 
-    
+    @GetMapping("/api/clientes/{id}")
+    public Cliente obterDadosCliente(@PathVariable Long id){
+        Cliente cliente = new Cliente(nome= "Alexsandra", cpf= "000.111.222.33");
+        return cliente;
+
+    }
 }
