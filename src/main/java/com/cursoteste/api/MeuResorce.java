d32e48e5dca9e286385a8b785206beb66d9687c5
alexsandraholanda <alexsandraale8795@gmail.com>
2020-11-04 00:12:29 -0300

    correcao

--- src/main/java/com/cursoteste/api/MeuResorce.java
+++ src/main/java/com/cursoteste/api/MeuResorce.java
@@ -1,14 +1,17 @@
 package com.cursoteste.api;
 
-import org.springframework.web.bind.annotation.*;
+import com.sun.javafx.binding.StringFormatter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MeuResorce {
 
     @GetMapping("/api/clientes/{id}")
-    public Cliente obterDadosCliente(@PathVariable Long id){
-        Cliente cliente = new Cliente(nome= "Alexsandra", cpf= "000.111.222.33");
+    public Cliente obterDadosCliente( @PathVariable Long id){
+        System.out.println(String.format("id recebido via url: %d", id));
+        Cliente cliente = new Cliente("teste", "123456");
         return cliente;
-
     }
 }
